098c0cb6f32cf28726adc3db7c85fd8827d645c4
GaneshKondri <92719287+GaneshKondri@users.noreply.github.com>
2022-12-02 14:05:33 +0530

    Update kl.py

--- kl.py
+++ kl.py
@@ -1,5 +1,5 @@
 # Implementation of Kernighan-Lin graph partitioning algorithm
-# Based on the paper: An Efficient Heuristic Procedure for Partitioning Graphs (https://ieeexplore.ieee.org/document/6771089)
+# Based on this paper: An Efficient Heuristic Procedure for Partitioning Graphs (https://ieeexplore.ieee.org/document/6771089)
 
 class Vertex:
     # id, edges, partition_label
