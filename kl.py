56fb79d1d8d529217e537aa9eb804b4d5c0b2850
maneeshmoturi <80633782+maneeshmoturi@users.noreply.github.com>
2022-12-02 23:51:05 +0530

    Update kl.py

--- kl.py
+++ kl.py
@@ -2,7 +2,8 @@
 # Based on the paper: An Efficient Heuristic Procedure for Partitioning Graphs (https://ieeexplore.ieee.org/document/6771089)
 
 class Vertex:
-    # id, edges, partition_label
+    # id, edges, partition_label 
+    #hiiii
     def __init__(self, id):
         self.id = id
         self.edges = []
